f94782f7f750390037a337c63267226ad647622e
davidelanfranchi <davide.lanfra@gmail.com>
2018-11-19 00:24:10 +0100

    Typography and main CTA

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -10,5 +10,14 @@ import Foundation from 'foundation-sites';
 
 $(document).foundation();
 
-import vhCheck from 'vh-check'
-const vhTest = vhCheck()
+import vhCheck from 'vh-check';
+const vhTest = vhCheck();
+
+import {TweenLite, ScrollToPlugin, Power4} from 'gsap/all';
+const plugins = [ScrollToPlugin, Power4];
+
+const $scrollToElements = $('[data-scroll-to]');
+$scrollToElements.click(function(e) {
+  e.preventDefault();
+	TweenLite.to(window, 1, { scrollTo: $(this).attr('href'), offsetY: 200, ease: Power4.easeOut});
+});
