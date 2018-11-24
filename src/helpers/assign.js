8d474fdc7369b72fb12b339d99e791d2db1ab023
davidelanfranchi <davide.lanfra@gmail.com>
2018-11-24 20:16:33 +0100

    Update 2018-11-24T19:16:31.289Z

--- src/helpers/assign.js
+++
@@ -1,7 +0,0 @@
-// https://stackoverflow.com/questions/24736938/is-it-possible-to-assign-a-parameter-value-within-handlebars-templates-without-u
-module.exports = function (varName, varValue, options) {
-	if (!options.data.root) {
-		options.data.root = {};
-	}
-	options.data.root[varName] = varValue;
-};
