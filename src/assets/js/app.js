6fbbcd08f70291e9d04c970bcdb4cde585c1e514
davidelanfranchi <davide.lanfra@gmail.com>
2018-11-24 23:57:05 +0100

    Fix class name

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,12 +3,12 @@ import whatInput from 'what-input';
 
 window.$ = $;
 
-import Foundation from 'foundation-sites';
+// import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
 
-$(document).foundation();
+// $(document).foundation();
 
 import vhCheck from 'vh-check';
 const vhTest = vhCheck();
